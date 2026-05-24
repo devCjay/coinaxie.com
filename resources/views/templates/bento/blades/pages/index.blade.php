b6e32d97e6cafe9a6d7910f09f452a7a8c58169e
devCjay <edehchijioke09@gmail.com>
2026-05-24 20:38:14 +0100

    update

--- resources/views/templates/bento/blades/pages/index.blade.php
+++ resources/views/templates/bento/blades/pages/index.blade.php
@@ -26,13 +26,13 @@
             'icon' =>
                 '<svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="12" r="10"/><path d="M16 8h-6a2 2 0 1 0 0 4h4a2 2 0 1 1 0 4H8"/><path d="M12 18V6"/></svg>',
         ],
-        <!-- [
+        [
             'title' => __('Forex'),
             'desc' => __('Major, Minor, and Exotic pairs with tight spreads.'),
             'module_loaded' => moduleEnabled('forex_module'),
             'icon' =>
                 '<svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><rect width="20" height="12" x="2" y="6" rx="2"/><circle cx="12" cy="12" r="2"/><path d="M6 12h.01M18 12h.01"/></svg>',
-        ], -->
+        ], 
     ];
 
     $standoutFeatures = [
