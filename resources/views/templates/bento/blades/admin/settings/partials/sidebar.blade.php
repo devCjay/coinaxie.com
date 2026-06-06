2a4a5b9716a6754ae8e7966273ef323216e76c65
devCjay <edehchijioke09@gmail.com>
2026-06-06 15:20:21 +0100

    update

--- resources/views/templates/bento/blades/admin/settings/partials/sidebar.blade.php
+++ resources/views/templates/bento/blades/admin/settings/partials/sidebar.blade.php
@@ -72,12 +72,7 @@
                 '<svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>',
         ],
 
-        [
-            'title' => 'Menu',
-            'route' => 'admin.settings.menu',
-            'icon' =>
-                '<svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M 4 12h16M4 18h16" /></svg>',   
-        ],
+       
         
         [
             'title' => 'Utilities',
