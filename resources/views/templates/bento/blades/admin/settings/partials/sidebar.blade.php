23e5833011eeab432bb654df87cc0065af93b910
devCjay <edehchijioke09@gmail.com>
2026-06-13 21:51:30 +0100

    update

--- resources/views/templates/bento/blades/admin/settings/partials/sidebar.blade.php
+++ resources/views/templates/bento/blades/admin/settings/partials/sidebar.blade.php
@@ -74,7 +74,7 @@
 
         [
        
-        title' => 'Menu',
+        'title' => 'Menu',
         'route' => 'admin.settings.menu',
             'icon' =>
                 '<svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" /></svg>',    
